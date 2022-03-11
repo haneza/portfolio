36cd271bffbb53217af4b196c4297b9126349a1f
Henrietta Zarvou <henriettazarvou@gmail.com>
2022-03-11 21:44:18 +0200

    files added

---
+++ scripts/app.js
@@ -0,0 +1,62 @@
+"use strict";
+
+//preloader
+window.addEventListener("load", () => {
+  document.querySelector(".js-preloader").classList.add("fade-out");
+  setTimeout(() => {
+    document.querySelector(".js-preloader").style.display = "none";
+  }, 600);
+});
+
+//nav
+const navToggler = document.querySelector(".js-nav-toggler");
+const nav = document.querySelector(".js-nav");
+
+function navToggle() {
+  nav.classList.toggle("active");
+  navToggler.classList.toggle("active");
+}
+
+navToggler.addEventListener("click", navToggle);
+
+//hide the nav by clicking outside of it
+document.addEventListener("click", (event) => {
+  const isClickInsideNav = nav.contains(event.target);
+  const isClickInsideNavToggler = navToggler.contains(event.target);
+  if (
+    !(isClickInsideNav || isClickInsideNavToggler) &&
+    nav.classList.contains("active")
+  ) {
+    navToggle();
+  }
+});
+
+// dark theme
+
+function themeLightDark() {
+  const switcherBtn = document.querySelector(".js-switcher-btn");
+  const icon = switcherBtn.querySelector("i");
+
+  function changeIcon() {
+    if (document.body.classList.contains("dark")) {
+      icon.classList.remove("fa-moon");
+      icon.classList.add("fa-sun");
+    } else {
+      icon.classList.remove("fa-sun");
+      icon.classList.add("fa-moon");
+    }
+  }
+
+  switcherBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    changeIcon();
+    if (document.body.classList.contains("dark")) {
+      localStorage.setItem("theme", "dark");
+    } else {
+      localStorage.setItem("theme", "light");
+    }
+  });
+
+  changeIcon();
+}
+themeLightDark();
